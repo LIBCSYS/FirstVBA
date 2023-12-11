b595b97ae23b66c6b7ff0b2697caaa6c87860b10
LIBCSYS <libcsystems@gmail.com>
2023-12-11 18:16:47 -0500

    Add project files.

---
+++ FirstVBA/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Windows
+
+Class MainWindow
+    Public Sub New()
+        InitializeComponent()
+        DisplayHelloWorld()
+    End Sub
+
+    Private Sub DisplayHelloWorld()
+        Dim helloLabel As New Label()
+        helloLabel.Content = "Welcome to LIBC Development Lab.  libcxyz@gmail.com"
+        Content = helloLabel
+    End Sub
+End Class
